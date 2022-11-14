c9a3bf60fed4072078338d1217d99b2c892f0dc5
depuits <colmanjoeri@gmail.com>
2022-11-14 09:12:20 +0100

    made extra config options optional

--- index.js
+++ index.js
@@ -6,78 +6,80 @@ const leds = require('pi-mqtt-led');
 const mqttClient = leds.mqtt;
 
 // DHT setup
-const dhtConf = config.get('dht');
-const dht = require('pigpio-dht');
-const sensor = dht(dhtConf.pin, dhtConf.type);
+if (config.has('dht')) {
+	const dhtConf = config.get('dht');
+	const dht = require('pigpio-dht');
+	const sensor = dht(dhtConf.pin, dhtConf.type);
 
-sensor.on('result', data => {
-	var dataJson = JSON.stringify(data);
-	mqttClient.publish(dhtConf.mqttTopic, dataJson);
-});
+	sensor.on('result', data => {
+		var dataJson = JSON.stringify(data);
+		mqttClient.publish(dhtConf.mqttTopic, dataJson);
+	});
 
-sensor.on('badChecksum', () => {
-	console.log('checksum failed');
-});
+	sensor.on('badChecksum', () => {
+		console.log('checksum failed');
+	});
 
-sensor.read();
-setInterval(() => {
 	sensor.read();
-}, dhtConf.pollInterval);
-
+	setInterval(() => {
+		sensor.read();
+	}, dhtConf.pollInterval);
+}
 
 
 // extras setup
-const Gpio = require('pigpio').Gpio;
-const extraConfs = config.get('extras');
+if (config.has('extras')) {
+	const Gpio = require('pigpio').Gpio;
+	const extraConfs = config.get('extras');
 
-for (let extra of extraConfs) {
+	for (let extra of extraConfs) {
 
-	const conf = extra;
-	const gpio = new Gpio(conf.pin, {mode: Gpio.OUTPUT});
+		const conf = extra;
+		const gpio = new Gpio(conf.pin, {mode: Gpio.OUTPUT});
 
-	const onState = conf.inverted ? 0 : 1;
-	const offState = conf.inverted ? 1 : 0;
+		const onState = conf.inverted ? 0 : 1;
+		const offState = conf.inverted ? 1 : 0;
 
 
-	function sendState()
-	{
-		mqttClient.publish(conf.mqttTopicState, gpio.digitalRead() == onState ? conf.stateOn : conf.stateOff, { retain: true });
-	}
-
-	function set(on) {
-		if (on)
+		function sendState()
 		{
-			gpio.digitalWrite(onState);
-		}
-		else
-		{
-			gpio.digitalWrite(offState);
+			mqttClient.publish(conf.mqttTopicState, gpio.digitalRead() == onState ? conf.stateOn : conf.stateOff, { retain: true });
 		}
 
-		sendState();
-	}
-
-	mqttClient.on('connect', function () {
-		mqttClient.subscribe(conf.mqttTopicSet);
-		set(false); // make sure the  is off
-	});
-
-	mqttClient.on('message', function (topic, message) {
-		if (topic === conf.mqttTopicSet) {
-			console.log('rec extra topic: ' + message);
-
-			if (message == conf.stateOn)
+		function set(on) {
+			if (on)
 			{
-				set(true);
+				gpio.digitalWrite(onState);
 			}
-			else if (message == conf.stateOff)
+			else
 			{
-				set(false);
+				gpio.digitalWrite(offState);
 			}
 
 			sendState();
 		}
-	});
 
+		mqttClient.on('connect', function () {
+			mqttClient.subscribe(conf.mqttTopicSet);
+			set(false); // make sure the  is off
+		});
+
+		mqttClient.on('message', function (topic, message) {
+			if (topic === conf.mqttTopicSet) {
+				console.log('rec extra topic: ' + message);
+
+				if (message == conf.stateOn)
+				{
+					set(true);
+				}
+				else if (message == conf.stateOff)
+				{
+					set(false);
+				}
+
+				sendState();
+			}
+		});
+	}
 }
 
